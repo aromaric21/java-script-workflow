8ade3df4fdd5c4bccdc5e48b56f8d391e1cd7b6f
aromaric <aromaricadjei213@gmail.com>
2025-03-17 11:58:29 +0000

    Functions

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-let myName = "Melvyn";
-console.log(myName.length > 10)
-console.log(myName.length > 5)
-console.log(myName.charAt(0) === "M")
-
-if (myName.length > 10 || (myName.length >5 && myName.charAt(0) === "M")){
-    console.log("0 > myAge <= 18");
-}else {
-    console.log("CONDITION EST FACILE");
+// 1. Functions
+// 2. Regular function
+// 3. Arrow function
+const name ="Melvyn";
+function sayHello(){
+   console.log(`Hello ${name}!`);
 }
+
+sayHello();
