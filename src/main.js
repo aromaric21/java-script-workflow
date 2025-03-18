6c5b97aeef1d74a410b199977bcda21d47a0b675
aromaric <aromaricadjei213@gmail.com>
2025-03-18 11:51:34 +0000

    D0M createElements

--- src/main.js
+++ src/main.js
@@ -2,19 +2,33 @@ import './style.css'
 
 const app = document.querySelector("#app");
 
+// Déconseillé
+app.innerHTML= `<div>
+<h1>
+Salut
+<h1>
+<input/>
+</div>`;
+
+// Conseillé
+const div = document.createElement("div");
 console.log({
-    parent: app.parentElement,
-    prevSister: app.previousElementSibling,
-    nextSister: app.nextElementSibling,
-    firstChild: app.firstElementChild,
-    lastChild: app.lastElementChild,
-    children: app.children,
+    parentElement: div.parentElement,
+    contenue: div.innerHTML,
 });
 
-document.getElementById
-// ->document.querySelector("#id")
-document.getElementsByClassName
-// ->document.querySelector("#.className")
-document.getElementsByTagName
-// ->document.querySelector("tagName")
+const title = document.createElement("h1");
+title.classList.add("big-title", "title");
+title.style.background= "blue";
+title.innerText = "Romaric !";
+div.appendChild(title);
+
+const input = document.createElement("input");
+div.appendChild(input);
+app.appendChild(div);
+
+app.appendChild(div);
+
+
+
 
